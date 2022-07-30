3f715c5895bb11ce07b196536ba705d6876ccd92
wuttinanhi <wuttinanhi@gmail.com>
2022-07-30 22:15:54 +0000

    try php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+echo "helloz";
+$test = true;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+</head>
+
+<body>
+    <br><br>
+    <?php
+    if ($test == true) {
+        echo "True";
+    } else {
+        echo "False";
+    }
+    ?>
+</body>
+
+</html>
